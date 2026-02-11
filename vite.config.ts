63e6a27569da977fd00bf8cd9e24ca2ebf8e12d5
Prasham Titiya <100197432+prasham1515@users.noreply.github.com>
2026-02-11 14:27:39 -0700

    Change base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/SAGE/",
+  base: "/sage/",
   server: {
     host: "::",
     port: 8080,
